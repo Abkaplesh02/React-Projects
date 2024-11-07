5a5fb5aa146b56bc6fda92c99e820dafcea48519
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-11-07 18:09:58 +0530

    update files

--- O1_Pomodoro_Project/src/App.jsx
+++ O1_Pomodoro_Project/src/App.jsx
@@ -1,34 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
 import './App.css'
 
+function padTime(time){
+  return time.toString().padStart(2,'0');
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [timeLeft , setTimeLeft]=useState(25*60);
+  const [title,setTitle]=useState('Timer')
+  const [isRunning,setRunning]=useState(false)
+  let IntervalId=useRef();
+
+  const Minutes = padTime(Math.floor(timeLeft/60));
+  const Seconds = padTime((timeLeft - Minutes*60));
+ 
+  
+
+  function handleClickStart(){
+    if(IntervalId.current != null) return;
+    setTitle("Timer running")
+    console.log("Timer started")
+    IntervalId.current=setInterval(() => {
+      setTimeLeft(timeLeft=>{
+        if(timeLeft>=1) return timeLeft-1;
+
+        handleClickReset();
+        return 0 ;
+      })
+    }, 1);
+    setRunning(true);
+  }
+
+  function handleClickStop(){
+    if(IntervalId.current==null)return;
+    setTitle('Pomodoro');
+    clearInterval(IntervalId.current);
+    IntervalId.current=null;
+    console.log("Timer paused")
+    setRunning(false);
+  }
+
+  function handleClickReset(){
+    setTitle('Ready to go another round')
+    console.log("Reset timer")
+    clearInterval(IntervalId.current);
+    IntervalId.current=null;
+   setTimeLeft(25*60);
+  }
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>
+        <h2>{title}</h2>
+        <div>
+          <span>
+            {Minutes}
+          </span>
+          <span>
+            ::
+          </span>
+          <span>
+            {Seconds}
+          </span>
+        </div>
+
+        <br />
+        <br />
+
+        <div >
+          {!isRunning && <button onClick={handleClickStart}>
+            Start
+            </button>}
+
+           {isRunning &&<button onClick={handleClickStop}>
+              Stop
+            </button>
+            } 
+            <button onClick={handleClickReset}>
+              Reset
+            </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
